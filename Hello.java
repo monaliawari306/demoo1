187962c0bc6f9c803df12c8fe94e6023e8dd33e7
monaliawari306 <monaliawari121@gmail.com>
2023-12-08 13:44:50 +0530

    features1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,23 @@
 public class Hello
 {
+
+  public void features1()
+   {
+        System.out.println("Feature 1");
+   }
+  public void features2()
+   {
+
+   }
+  public void features3()
+   {
+
+   }
+  public void features4()
+   {
+
+   }
+
   public static void main(String args[])
 
 {
